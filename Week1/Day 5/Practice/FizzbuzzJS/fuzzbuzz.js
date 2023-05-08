461ae9e7416104f0f872906cb9c7281bde4b56b6
BENAMORNAWFAL <technobenamor@gmail.com>
2023-05-08 14:15:08 +0100

    FuzzBuzz number

---
+++ Week1/Day 5/Practice/FizzbuzzJS/fuzzbuzz.js
@@ -0,0 +1,12 @@
+function verif(){
+    for(var x=1;x<=100;x++){
+        if(x % 3==0 && x % 5==0){
+            console.log("FizzBuzz");
+        }else if(x % 3==0){
+            console.log("Fizz");
+        }else if(x % 5==0){
+            console.log("Buzz");
+        }else{console.log(x)}
+    }
+}
+verif();
